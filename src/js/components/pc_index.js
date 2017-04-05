58b49c3cc15ae5b2652b5b8598607244732a9088
Chenjie Zhou <czhou33@ucsc.edu>
2017-04-04 22:54:27 -0700

    [feat] Content page completed !

--- src/js/components/pc_index.js
+++ src/js/components/pc_index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import PCHeader from './pc_header.js'
-import PCFooter from './pc_footer.js'
-
+import PCHeader from './pc_header.js';
+import PCFooter from './pc_footer.js';
+import PCNewsContainer from './pc_newscontainer.js';
 
 export default class PCIndex extends React.Component {
   render() {
     return(
       <div>
         <PCHeader/>
+        <PCNewsContainer/>
         <PCFooter/>
       </div>
     );
