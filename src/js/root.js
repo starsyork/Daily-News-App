3d5cf041830facd009906c4980fa6085fee9a9b7
Chenjie Zhou <czhou33@ucsc.edu>
2017-04-13 01:15:57 -0700

    [feat] PC news detail page

--- src/js/root.js
+++ src/js/root.js
@@ -6,13 +6,17 @@ import PCIndex from './components/pc_index.js';
 import MobileIndex from './components/mobile_index.js';
 import 'antd/dist/antd.css';
 import MediaQuery from 'react-responsive';
+import PCNewsDetails from './components/pc_news_detail';
 
 export default class Root extends React.Component{
   render(){
     return(
       <div>
         <MediaQuery query='(min-device-width: 1224px)'>
-          <PCIndex/>
+          <Router history={hashHistory}>
+						<Route path="/" component={PCIndex}></Route>
+						<Route path="/details/:uniquekey" component={PCNewsDetails}></Route>
+					</Router>
         </MediaQuery>
         <MediaQuery query='(max-device-width: 1224px)'>
           <MobileIndex/>
