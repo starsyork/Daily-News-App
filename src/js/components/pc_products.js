272e66fff56e66fc759286f61745ded4536a4310
Chenjie Zhou <czhou33@ucsc.edu>
2017-04-16 22:32:31 -0700

    [feat] Product side bar, copy from qq.com 😳

---
+++ src/js/components/pc_products.js
@@ -0,0 +1,188 @@
+import React from 'react';
+
+export default class PCProduct extends React.Component {
+    render() {
+        return (
+            <div className="rightArea" id="pronav">
+              <div className="rightProduct" id="productNav" bosszone="pronav1">
+                <div className="productLine01" bosszone="proarea1">
+                  <a href="http://im.qq.com/" target="_blank" id="qqProduct" style={{
+                            marginRight: 15
+                  }}>QQ</a>
+                  <a href="http://mail.qq.com/" target="_blank" style={{
+                            marginRight: 15
+                  }}>邮箱</a>
+                  <a href="http://vip.qq.com/?ADTAG=www.qq.com" target="_blank" style={{
+                            marginRight: 16
+                  }}>会员</a>
+                  <a href="http://pcmgr.qq.com/?ADTAG=tr.pcmgr.qqcom.QQCOM" target="_blank" style={{
+                            marginRight: 16
+                  }}>电脑管家</a>
+                  <a href="http://pc.qq.com/" target="_blank" style={{
+                            margin: 0
+                  }}>软件</a>
+                  <iframe id="tmp_downloadhelper_iframe" style={{
+                            display: 'none'
+                  }}/></div>
+                <div className="productLine02" bosszone="proarea2">
+                  <a href="http://weixin.qq.com/" target="_blank" style={{
+                            marginRight: 14
+                  }}>微信</a>
+                  <a href="http://qzone.qq.com/" target="_blank" style={{
+                            marginRight: 14
+                  }}>空间</a>
+                  <a href="http://ke.qq.com/index.html?from=72" target="_blank" style={{
+                            marginRight: 13
+                  }}>课堂</a>
+                  <a href="http://film.qq.com/" target="_blank" style={{
+                            marginRight: 14
+                  }}>视频VIP</a>
+                  <a href="http://www.qcloud.com/?from=txwsy" target="_blank" style={{
+                            margin: 0
+                  }}>腾讯云</a>
+                </div>
+                <div className="productLine03" bosszone="proarea3">
+                  <a href="http://kuaibao.qq.com/" target="_blank" style={{
+                            marginRight: 17
+                  }} className="Q-red">快报</a>
+                  <a href="http://news.qq.com/mobile/" target="_blank" style={{
+                            marginRight: 17
+                  }} className="Q-red">新闻APP</a>
+                  <a href="http://finance.qq.com/products/portfolio/" target="_blank" style={{
+                            marginRight: 19
+                  }}>自选股</a>
+                  <a href="http://sports.qq.com/kbs/" target="_blank" style={{
+                            margin: 0
+                  }}>体育APP</a><br/>
+                  <a href="http://v.qq.com/download.html#pc" target="_blank" style={{
+                            marginRight: 17
+                  }}>视频</a>
+                  <a href="http://m.qq.com/" target="_blank" style={{
+                            marginRight: 17
+                  }}>手机管家</a>
+                  <a href="http://browser.qq.com/" target="_blank" style={{
+                            marginRight: 19
+                  }}>浏览器</a>
+                  <a href="http://android.myapp.com/?g_f=990371" target="_blank" style={{
+                            margin: 0
+                  }}>应用宝</a>
+                </div>
+                <div className="productLine04" bosszone="proarea4">
+                  <a href="http://lol.qq.com/index.shtml?ADTAG=media.innerenter.qqcom.index_navigation" target="_blank" style={{
+                            marginRight: 16
+                  }}>LOL</a>
+                  <a href="http://cf.qq.com/?ADTAG=media.innerenter.qqcom.index_navigation" target="_blank" style={{
+                            marginRight: 18
+                  }}>CF</a>
+                  <a href="http://dnf.qq.com/?ADTAG=media.innerenter.qqcom.index_navigation" target="_blank" style={{
+                            marginRight: 16
+                  }}>DNF</a>
+                  <a href="http://dn.qq.com/?ADTAG=media.innerenter.qqcom.index_navigation" target="_blank" style={{
+                            marginRight: 20
+                  }}>龙之谷</a>
+                  <a href="http://wuxia.qq.com/?ADTAG=media.innerenter.qqcom.index_navigation" target="_blank" style={{
+                            margin: 0
+                  }}>天刀</a><br/>
+                  <a href="http://iwan.qq.com/index.htm?ADTAG=media.innerenter.qqcom.indexnavigation" target="_blank" style={{
+                            marginRight: 12
+                  }}>爱玩</a>
+                  <a href="http://nz.qq.com/?ADTAG=media.innerenter.qqcom.indexnavigation" target="_blank" style={{
+                            marginRight: 12
+                  }}>逆战</a>
+                  <a href="http://mhzx.qq.com/?ADTAG=media.innerenter.qqcom.index_navigation" target="_blank" style={{
+                            marginRight: 9
+                  }}>梦诛手游</a>
+                  <a href="http://huoying.qq.com/act/a20141009landingpage/index.htm?via=45&ADTAG=ied.neiguang&ADTAG=media.innerenter.qqcom.index_navigation" target="_blank" style={{
+                            marginRight: 10
+                  }}>火影OL</a>
+                  <a href="http://pvp.qq.com/?ADTAG=media.innerenter.qqcom.index_navigation" target="_blank" style={{
+                            margin: 0
+                  }}>王者</a>
+                </div>
+                <div className="productLine05" bosszone="proarea5">
+                  <a href="http://dc2.jd.com/auto.php?service=transfer&type=dmp&from=dmp&kid=77&klid=11727&to=http://www.jd.com" target="_blank" style={{
+                            marginRight: 15
+                  }}>京东</a>
+                  <a href="https://qian.qq.com?stat_data=oth87ppcsy00222&ADTAG=SCQD.BD.PC.TXDH1" target="_blank" style={{
+                            marginRight: 15
+                  }}>理财通</a>
+                  <a href="http://fm.qq.com/" target="_blank" style={{
+                            marginRight: 13
+                  }}>企鹅FM</a>
+                  <a href="http://pay.qq.com/" target="_blank" style={{
+                            marginRight: 12
+                  }}>Q币</a>
+                  <a href="http://www.qq.com/map/" target="_blank" style={{
+                            margin: 0
+                  }}>全部</a>
+                </div>
+                <div className="productFloatLayout" id="productFloatLayout" style={{
+                        width: 14,
+                        right: 279
+                }}>
+                  <div className="productFloat" id="productFloat" bosszone="pronav2" style={{
+                            display: 'none',
+                            width: 0
+                  }}>
+                    <div className="productLine01" bosszone="ufpro1">
+                      <a href="http://www.qq.com/map/" target="_blank">更多</a>
+                      <a href="http://www.weiyun.com/index.html" target="_blank">微云</a>
+                      <a href="http://image.qq.com/" target="_blank">QQ影像</a>
+                      <a href="http://player.qq.com/" target="_blank">QQ影音</a>
+                      <a href="http://py.qq.com/" target="_blank">QQ拼音</a>
+                      <a href="http://xf.qq.com/" target="_blank">QQ旋风</a>
+                    </div>
+                    <div className="productLine02" bosszone="ufpro2">
+                      <a href="http://xing.qq.com/" target="_blank">星钻</a>
+                      <a href="http://vip.qzone.com/" target="_blank">黄钻</a>
+                      <a href="http://vip.music.qq.com/" target="_blank">绿钻</a>
+                      <a href="http://show.qq.com/" target="_blank">红钻</a>
+                      <a href="http://mini.qq.com/download/" target="_blank">腾讯网</a>
+                      <a href="http://show.qq.com/" target="_blank">QQ秀</a>
+                      <a href="http://shaka.qq.com/" target="_blank">闪咖</a>
+                    </div>
+                    <div className="productLine03" bosszone="ufpro3">
+                      <a href="http://g.qq.com?ADTAG=pcqq.home.sidenav" target="_blank">手游宝</a>
+                      <a href="http://z.qzone.com/" target="_blank">手机QQ空间</a>
+                      <a href="http://kg.qq.com/" target="_blank">全民K歌</a>
+                      <a href="http://y.qq.com/" target="_blank">QQ音乐</a>
+                      <a href="http://book.qq.com/?g_f=70085" target="_blank">QQ阅读</a><br/>
+                      <a href="http://www.weishi.com/" target="_blank">微视</a>
+                      <a href="http://pim.qq.com/?g_f=6661" target="_blank">同步助手</a>
+                      <a href="http://map.qq.com/lubao/?source_id=10019" target="_blank">路宝</a>
+                      <a href="http://act.qzone.qq.com" target="_blank">黄钻活动</a>
+                      <a href="http://gaopeng.qq.com/" target="_blank">QQ团购</a>
+                      <a href="http://go.qq.com/?attach=qqlvyou_28_13_1" target="_blank">QQ旅游</a>
+                    </div>
+                    <div className="productLine04" bosszone="ufpro4">
+                      <a href="http://cfm.qq.com/?ADTAG=media.innerenter.qqcom.index_navigation" target="_blank">CF手游</a>
+                      <a href="https://cqsj.qq.com/web201701/main.shtml?ADTAG=media.innerenter.qqcom.index_navigation" target="_blank" style={{
+                                    marginRight: 9
+                      }}>传世手游</a>
+                      <a href="http://speed.qq.com/?ADTAG=media.innerenter.qqcom.indexnavigation" target="_blank">飞车</a>
+                      <a href="http://eafifa.qq.com/?ADTAG=media.innerenter.qqcom.index_navigation" target="_blank">FIFA</a>
+                      <a href="http://nba2k.qq.com/?ADTAG=media.innerenter.qqcom.index_navigation">NBA2K</a>
+                      <a href="http://feiji.qq.com/cp/a20161223ztz/index.shtml?ADTAG=media.innerenter.qqcom.index_navigation" target="_blank">飞机大战</a><br/>
+                      <a href="http://yl.qq.com/?ADTAG=media.innerenter.qqcom.index_navigation" target="_blank">御龙</a>
+                      <a href="http://hyrz.qq.com/?ADTAG=media.innerenter.qqcom.indexnavigation" target="_blank">火影手游</a>
+                      <a href="http://bns.qq.com/main.shtml?ADTAG=media.innerenter.qqcom.index_navigation" target="_blank">剑灵</a>
+                      <a href="https://jxqy.qq.com/main.shtml?ADTAG=media.innerenter.qqcom.index_navigation" target="_blank">剑侠</a>
+                      <a href="http://x5.qq.com/?ADTAG=media.innerenter.qqcom.indexnavigation" target="_blank">炫舞</a>
+                      <a href="http://5s.qq.com/main.shtml?ADTAG=media.innerenter.qqcom.index_navigation" target="_blank">炫舞时代</a>
+                    </div>
+                    <div className="productLine05" bosszone="ufpro5">
+                      <a href="http://www.yixun.com/" target="_blank">易迅网</a>
+                      <a href="http://meishi.qq.com/" target="_blank">美食</a>
+                      <a href="http://www.gaopeng.com" target="_blank">高朋网</a>
+                      <a href="http://piao.qq.com/" target="_blank">QQ电影票</a>
+                      <a href="http://cb.qq.com/?attach=200_1000_10090&QQ_from=200_1000_10090" target="_blank">彩贝</a>
+                      <a href="http://888.qq.com/?bc_tag=10161.1.1" target="_blank">QQ彩票</a>
+                    </div>
+                  </div>
+                  <div className="productFloatBtn" id="productFloatBtn" bosszone="prounford"/>
+                </div>
+              </div>
+            </div>
+        );
+    }
+}
